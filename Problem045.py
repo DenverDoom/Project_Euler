4dc8bf31da2c4adec24af554612979dcdc0bcbe0
DenverDoom <sayandipdey91@gmail.com>
2025-05-13 22:21:39 +0530

    problem102

--- Problem045.py
+++ Problem045.py
@@ -1,20 +1,21 @@
-import sympy.core.numbers
-from sympy import symbols, solve, Eq
 def hexagon_number(n):
-    return n*(2*n-1)
+    return n * (2 * n - 1)
+
+
 def is_pentagon_number(num):
-    n = (1 + (1 + 24 * num) ** 0.5)/6
+    n = (1 + (1 + 24 * num) ** 0.5) / 6
     n_int = int(n)
     if n == n_int:
         return True
     else:
         return False
 
+
 limit = 30000
 tri_penta_hexa_list = []
 for i in range(144, limit):
     temp = hexagon_number(i)
-    if(is_pentagon_number(temp)):
+    if is_pentagon_number(temp):
         tri_penta_hexa_list.append(temp)
         print(f'Found Triangle Number: {temp}')
         break
